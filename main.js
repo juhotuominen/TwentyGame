35a822b1b5e36e2c85fe933325cdc4ab9d466d35
juhotuominen <juhonen00@gmail.com>
2023-04-25 16:54:30 +0300

    simplified

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-const button = document.getElementById("button");
-button.addEventListener("click", randomize);
 const setButton = document.getElementById("setButton");
 setButton.addEventListener("click", setNumber);
 
@@ -8,16 +6,20 @@ setButton.addEventListener("click", setNumber);
 function randomize(){
     const number = Math.floor(Math.random() * 1000);
     document.getElementById('randomnumber').innerHTML = number;
-    
 }
 
 function setNumber(){
+    const playedNumbers = [];
     var number = document.getElementById('randomnumber').innerHTML;
     var location = document.getElementById("location").value;
-    console.log(location, number);
     document.getElementById('box' + location).innerHTML = number;
     document.getElementById("box" + location).className = "set";
+    playedNumbers[location] = number;
+    console.log(playedNumbers[location]);
+    randomize();
 }
 
+
+  
 
 
