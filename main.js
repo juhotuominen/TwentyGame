2647ed3338c37ca5d8ca28f60c79bfe43ea9065e
juhotuominen <juhonen00@gmail.com>
2023-04-25 17:03:15 +0300

    add array to see if numbers are in correct order

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 
 const setButton = document.getElementById("setButton");
 setButton.addEventListener("click", setNumber);
-
+const playedNumbers = [];
 
 function randomize(){
     const number = Math.floor(Math.random() * 1000);
     document.getElementById('randomnumber').innerHTML = number;
 }
 
+function arrayInIncreasingOrder(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 function setNumber(){
-    const playedNumbers = [];
     var number = document.getElementById('randomnumber').innerHTML;
     var location = document.getElementById("location").value;
     document.getElementById('box' + location).innerHTML = number;
     document.getElementById("box" + location).className = "set";
     playedNumbers[location] = number;
-    console.log(playedNumbers[location]);
-    randomize();
+    console.log(playedNumbers);
+
+    if (arrayInIncreasingOrder(playedNumbers)){
+        randomize();
+      } else {
+        document.getElementById('randomnumber').innerHTML = "Lost";
+      }
+      
 }
 
 
